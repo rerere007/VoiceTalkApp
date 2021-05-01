dbb1742614ae4df6881db46747022234d911d7bf
岩井聡一朗 <soichiro@iwaisatoshiichirounoMacBook-puro.local>
2021-05-01 17:23:59 +0900

    create app

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+// プラグインを利用するためにwebpackを読み込んでおく
+const webpack = require('webpack');
+
+// output.pathに絶対パスを指定する必要があるため、pathモジュールを読み込んでおく
+const path = require('path');
+
+module.exports = {
+  //ファイル更新の監視
+  watch: true,
+  // モードの設定、v4系以降はmodeを指定しないと、webpack実行時に警告が出る
+  mode: 'development',
+  // エントリーポイントの設定
+  entry: [
+    './src/js/hello.js'
+  ],
+  // 出力の設定
+  output: {
+    // 出力するファイル名
+    filename: 'bundle.js',
+    // 出力先のパス（絶対パスを指定する必要がある）
+    path: path.join(__dirname, 'public/js')
+  },
+  // ローダーの設定
+  module: {
+    rules: [
+      {
+        // ローダーの処理対象ファイル
+        test: /\.js$/,
+        // ローダーの処理対象から外すディレクトリ
+        exclude: /node_modules/,
+        use: [
+          {
+            // 利用するローダー
+            loader: 'babel-loader',
+            // ローダーのオプション
+            // 今回はbabel-loaderを利用しているため
+            // babelのオプションを指定しているという認識で問題ない
+            options: {
+              presets: [['@babel/preset-env', { modules: false }]]
+            }
+          }
+        ]
+      },
+      {
+        // enforce: 'pre'を指定することによって
+        // enforce: 'pre'がついていないローダーより早く処理が実行される
+        // 今回はbabel-loaderで変換する前にコードを検証したいため、指定が必要
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      }
+    ]
+  }
+};
